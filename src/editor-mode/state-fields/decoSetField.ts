428dc4bd6ad7987c1384466ba4d6b3bf3dd5e157
kotaindah55 <kotaindah55@gmail.com>
2025-03-07 14:01:05 +0200

    style: add doc comment to the decoSetField

--- src/editor-mode/state-fields/decoSetField.ts
+++ src/editor-mode/state-fields/decoSetField.ts
@@ -2,6 +2,7 @@ import { StateField } from "@codemirror/state";
 import { builderField } from "src/editor-mode/state-fields";
 import { EditorView } from "@codemirror/view";
 
+/** Stores only height-altering decorations. */
 export const decoSetField = StateField.define({
     create(state) {
         let builder = state.field(builderField);
